65522dd9018b910a38114589478aa649cd724258
Matteo Atti <attuch@gmail.com>
2012-05-25 13:23:05 +0200

    add comment in order_by. closed #29

--- django/contrib/aderit/generic_utils/templatetags/order_by.py
+++ django/contrib/aderit/generic_utils/templatetags/order_by.py
@@ -7,6 +7,10 @@ def order_by(queryset, args):
     args = [x.strip() for x in args.split(',')]
     return queryset.order_by(*args)
 
+
+"""
 {% load order_by %}
 
 {% for item in your_list|order_by:"field1,-field2,other_class__field_name" %}
+
+"""
