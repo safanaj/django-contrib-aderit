af4639a8ef627a602d692caf6047d0c3d199653a
Matteo Atti <attuch@gmail.com>
2012-06-21 12:54:32 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 
 setup(name='ContribAderit',
-	version='1.0',
+	version='1.1',
 	description='Collection of Aderit tools for Django',
 	author='Matteo Atti',
 	author_email='matteo.atti@aderit.it',
-	packages=['django.contrib.aderit.send_mail','django.contrib.aderit.generic_utils','django.contrib.aderit.generic_utils.templatetags','django.contrib.aderit.generic_utils.templates'],
+	packages=['django.contrib.aderit',
+		  'django.contrib.aderit.send_mail',
+		  'django.contrib.aderit.generic_utils',
+		  'django.contrib.aderit.generic_utils.templatetags',
+		  'django.contrib.aderit.generic_utils.templates'],
 	requires=['Django (>=1.3)']
 )
