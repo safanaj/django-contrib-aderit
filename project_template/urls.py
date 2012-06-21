6bc3a28d4e1d3d294a6949e454b421bd1fe3e946
Matteo Atti <attuch@gmail.com>
2012-06-21 18:22:14 +0200

    trivial fix typos

--- project_template/urls.py
+++ project_template/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import patterns, include, url
+#from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import *
 from django.views.generic import TemplateView, ListView, DetailView
 from django.conf import settings
 
