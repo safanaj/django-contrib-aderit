69ae7b8d822e67533fabdec4a6447b373ae93b3d
Matteo Atti <attuch@gmail.com>
2012-06-28 18:08:56 +0200

    modifiche nel setup, ma ancora incompleto

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
 
 setup(name='DjangoContribAderit',
-	version='1.2',
+	version='1.3',
 	description='Collection of Aderit tools for Django',
 	author='Matteo Atti',
 	author_email='matteo.atti@aderit.it',
 	packages=['django.contrib.aderit',
 		  'django.contrib.aderit.send_mail',
+		  'django.contrib.aderit.access_account',
+		  'django.contrib.aderit.access_account.templates',
 		  'django.contrib.aderit.generic_utils',
 		  'django.contrib.aderit.generic_utils.templatetags',
 		  'django.contrib.aderit.generic_utils.templates'],
