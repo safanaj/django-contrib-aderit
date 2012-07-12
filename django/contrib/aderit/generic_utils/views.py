423ad71a49cc5aa54fee0d6c74f3be623a80fb30
Matteo Atti <attuch@gmail.com>
2012-07-12 11:58:10 +0200

    added external views

--- django/contrib/aderit/generic_utils/views.py
+++ django/contrib/aderit/generic_utils/views.py
@@ -1,1 +1,14 @@
-# empty
+from django.http import HttpResponsePermanentRedirect
+
+def external_view(request, target):
+    return HttpResponsePermanentRedirect('http://' + target)
+
+## USAGE
+## (comodo per django cms nelle impostazioni avanzate, il redirect ad una pagina)
+## in urls.py 
+## from django.contrib.aderit.generic_utils.views import external_view
+
+## urlpatterns = (
+## url(r'^offsite/(?P<target>.+)$', external_view),
+## )
+
