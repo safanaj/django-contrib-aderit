d02834bdabe26f2140e9e8bd530409f724a99491
Marco Bardelli <fanaj@jmini>
2012-10-01 20:49:56 +0200

    added a GenericUtilView to setup attrs in View instance

--- django/contrib/aderit/generic_utils/views/__init__.py
+++ django/contrib/aderit/generic_utils/views/__init__.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponsePermanentRedirect
-
+from django.views.generic import View
 def external_view(request, target):
     return HttpResponsePermanentRedirect('http://' + target)
 
 ## USAGE
 ## (comodo per django cms nelle impostazioni avanzate, il redirect ad una pagina)
-## in urls.py 
+## in urls.py
 ## from django.contrib.aderit.generic_utils.views import external_view
 
 ## urlpatterns = (
@@ -14,6 +14,15 @@ def external_view(request, target):
 
 from autologin import AutoLoginView
 
+class GenericUtilView(View):
 
-__all__ = ['external_view',
-           'AutoLoginView']
+    force_setup_attrs = False
+
+    def _setup_attrs(self, **kwargs):
+        for k in kwargs.keys():
+            if self.force_setup_attrs or hasattr(self, k):
+                setattr(self, k, kwargs.get(k))
+
+    def dispatch(self, request, *args, **kwargs):
+        self._setup_attrs(**self.kwargs)
+        return super(GenericUtilView, self).dispatch(request, *args, **kwargs)
