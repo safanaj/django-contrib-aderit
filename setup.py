f5c1540e8f6080db884d38bb3cb7802739ccbbed
Marco Bardelli <marco.bardelli@aderit.it>
2012-10-11 16:58:38 +0200

    * [FIX] setup.py copied from django  * don't put __init__.py under templates and data_files dir in general  * BUG: impossible to name a "tags" dir for data_files

--- setup.py
+++ setup.py
@@ -2,31 +2,44 @@ from distutils.core import setup
 from distutils.sysconfig import get_python_lib
 import os
 
-PKGS = ['django.contrib.aderit',
-        'django.contrib.aderit.send_mail',
-        'django.contrib.aderit.access_account',
-        'django.contrib.aderit.generic_utils',
-        'django.contrib.aderit.generic_utils.views',
-        'django.contrib.aderit.generic_utils.autologin',
-        'django.contrib.aderit.generic_utils.templatetags']
+### cmdclasses = {'install_data': install_data} ### what is this ?
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+django_contrib_aderit_root_dir = 'django'
 
-def get_data_files():
-    _data_files_ = []
-    for p in PKGS:
-        founds = []
-        dirp = p.replace('.','/')
-        founds = os.popen2("find %s -type f -name '*.html'" % dirp)[1].read().split()
-        if len(founds) > 0:
-            #_data_files_.append((os.path.join(get_python_lib(), dirp, 'templates', os.path.basename(dirp)), founds))
-            _data_files_.append((os.path.join(get_python_lib(), os.path.dirname(founds[0])), founds))
-    return _data_files_
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+for dirpath, dirnames, filenames in os.walk(django_contrib_aderit_root_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([os.path.join(get_python_lib(), dirpath), [os.path.join(dirpath, f) for f in filenames]])
 
 setup(name='DjangoContribAderit',
       version='1.4.0',
       description='Collection of Aderit tools for Django',
       author='Matteo Atti',
       author_email='matteo.atti@aderit.it',
-      packages=PKGS,
-      data_files=get_data_files(),
+      packages=packages,
+      data_files=data_files,
       requires=['Django (>=1.3)']
 )
