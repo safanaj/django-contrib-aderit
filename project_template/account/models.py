43f11c781396a87e966c1ea70ed1b55631ea2c43
Marco Bardelli <marco.bardelli@aderit.it>
2012-11-30 18:44:25 +0100

    [enhance] project_template/account.models use GenericPhoneField from generic_utils

--- project_template/account/models.py
+++ project_template/account/models.py
@@ -10,6 +10,7 @@ from django.utils.log import getLogger
 from django.utils.translation import ugettext_lazy as _
 
 from django.contrib.aderit.access_account.models import AccessAccount
+from django.contrib.aderit.generic_utils.models.fields import GenericPhoneField as PhoneField
 
 logger = getLogger('account.models')
 
@@ -17,8 +18,8 @@ phone_regex = r'^\+?[0-9]{3}[0-9 ]{5,18}$'
 
 ### User Profile Model
 class Account(AccessAccount):
-    phone = models_fields.CharField(_("Phone"), validators=[RegexValidator(regex=re.compile(phone_regex))], max_length=125, blank=True) # telefono fisso
-    mobile_phone = models_fields.CharField(_("Mobile Phone"), validators=[RegexValidator(regex=re.compile(phone_regex))], max_length=125, blank=True) # telefono mobile
+    phone = PhoneField(_("Phone"), max_length=125, blank=True) # telefono fisso
+    mobile_phone = PhoneField(_("Mobile Phone"), max_length=125, blank=True) # telefono mobile
     company = models_fields.CharField(_("Company"), max_length=125, blank=True) # ragione sociale
     address = models_fields.CharField(_("Address"), max_length=125, blank=True) # indirizzo di fatturazione
     zip_code = models_fields.CharField(_("Zip Code"), max_length=125, blank=True) # cap
